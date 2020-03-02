0a5ea08a7531084b3c71b34bf60c80eeb98f6d05
ylu-git <yuan.lu@icloud.com>
2020-03-01 19:23:20 -0500

    Upload website

---
+++ assets/js/tooltip.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  //  Activate the Tooltips
+  $('[data-toggle="tooltip"], [rel="tooltip"]').tooltip();
+
+  // Activate Popovers and set color for popovers
+  $('[data-toggle="popover"]').each(function() {
+    color_class = $(this).data('color');
+    $(this).popover({
+      template: '<div class="popover popover-' + color_class + '" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>'
+    });
+  });
+
+});
